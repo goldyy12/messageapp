76fda681106793a2471a739f96906afbf2655370
Diar <diarselmani10@gmail.com>
2026-01-14 20:40:52 +0100

    fix

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,8 +1,8 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import AuthProvider from "./context/authProvider";
-import { BrowserRouter } from "react-router-dom"; // ✅ add this
+import AuthProvider from "./context/authProvider.jsx"; 
+import { BrowserRouter } from "react-router-dom"; 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
