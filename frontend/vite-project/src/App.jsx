8313c84b7e1754340ff2f26fa2f337e671d57714
Diar <diarselmani10@gmail.com>
2026-01-14 21:01:33 +0100

    fixxx

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./components/layout";
 import ProtectedRoute from "./components/protectedRoutes";
 
 import Login from "./pages/Login";
