8e7cd149e20ed6311fafcae58802f5054756d390
Diar <diarselmani10@gmail.com>
2026-01-14 21:08:36 +0100

    fixxx

--- frontend/vite-project/src/pages/Signup.jsx
+++ frontend/vite-project/src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/authContext.jsx";
 import api from "../api";
-import styles from "../styles/Login.module.css"; // ✅ Shares the same styling
+import styles from "../styles/login.module.css"; 
 
 export default function Register() {
   const [username, setUsername] = useState("");
