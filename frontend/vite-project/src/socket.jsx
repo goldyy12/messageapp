2b71f84496a558841a54302a35ae261f9ce18db4
Diar <diarselmani10@gmail.com>
2026-04-08 17:48:44 +0200

    fix

--- frontend/vite-project/src/socket.jsx
+++ frontend/vite-project/src/socket.jsx
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://messageapp-dj2x.onrender.com", {
-  transports: ["websocket", "polling"], // Allow fallback to polling
+const socket = io(import.meta.env.VITE_API_URL, {
   withCredentials: true,
+  transports: ["polling", "websocket"],
 });
+
+console.log("Connecting to socket at:", import.meta.env.VITE_API_URL);
+
 export default socket;
