b68e4fd8409bb6a4991ef1dfdf943d73a44fa62f
Diar <diarselmani10@gmail.com>
2026-05-02 01:15:30 +0200

    redis

--- backend/server.ts
+++ backend/server.ts
@@ -21,7 +21,7 @@ const io = new Server<clientToServer, serverToClient, SocketData>(server, {
     credentials: true,
   },
 });
-
+connectRedis();
 const onlineUsers = new Map();
 
 io.on("connection", (socket) => {
@@ -44,6 +44,7 @@ io.on("connection", (socket) => {
 });
 
 import { type Request, type Response, type NextFunction } from "express";
+import { connectRedis } from "./lib/redis.js";
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error("Global error handler:", err);
