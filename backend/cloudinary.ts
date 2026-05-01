b7f77a051bf258180d666334fbef4090ec05a394
Diar <diarselmani10@gmail.com>
2026-05-01 19:15:12 +0200

    fixes

--- backend/cloudinary.ts
+++ backend/cloudinary.ts
@@ -8,7 +8,7 @@ const storage = new CloudinaryStorage({
     folder: "chat_uploads",
     allowed_formats: ["jpg", "png", "jpeg", "gif", "pdf"],
     resource_type: "auto",
-    type: "private",
+    type: "upload",
   } as any,
 });
 
