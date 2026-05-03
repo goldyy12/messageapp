11b2b65c76e8c2ebbaa6d3a1d14b4f8d725f998f
Diar <diarselmani10@gmail.com>
2026-05-03 21:49:03 +0200

    Cors

--- frontend/vite-project/src/socket.tsx
+++ frontend/vite-project/src/socket.tsx
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_API_URL, {
   withCredentials: true,
-  transports: ["polling", "websocket"],
+  transports: ["websocket"],
 });
 
 console.log("Connecting to socket at:", import.meta.env.VITE_API_URL);
